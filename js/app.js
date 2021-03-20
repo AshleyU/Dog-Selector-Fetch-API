a7c1d617bd1e125f1d772e5fbdbf27441d1c0f83
Ashley U <ashley.umetsu@gmail.com>
2021-03-20 16:35:56 -0500

    Added fetch to pull resources

--- js/app.js
+++ js/app.js
@@ -6,13 +6,25 @@ const form = document.querySelector('form');
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
+fetch('https://dog.ceo/api/breeds/list')
+  .then(response => response.json())
+  .then(data => console.log(data))
 
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(data => generateImage(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-
+function generateImage(data) {
+  const html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+    `;
+  card.innerHTML = html;
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
